f1a83918da6338cd30731ae0ab8d8c259a0bc324
kusuhema <149591529+kusuhema@users.noreply.github.com>
2024-04-21 18:20:13 -0500

    change in signin page

--- project_H/src/Components/Siginup/Signin.js
+++ project_H/src/Components/Siginup/Signin.js
@@ -1,243 +1,42 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
 
-// import React, { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import './Sigin.css';
-
-// const Signin = () => {
-//     const navigate = useNavigate();
-//     const [data, setData] = useState({
-//         username: "",
-//         password: "",
-//     });
-
-//     const { username, password } = data;
-
-//     const changeHandler = (e) => {
-//         setData({ ...data, [e.target.name]: e.target.value });
-//     }
-
-//     const Add = [
-//         {
-//             username: 'Admin',
-//             password: 10101,
-//         }
-//     ];
-
-//     const submitHandler = (event) => {
-//         event.preventDefault();
-//         const New = Add.find(user => user.username === username && user.password.toString() === password);
-//         const storedData = JSON.parse(localStorage.getItem('userData'));
-
-//         if (New) {
-//             navigate('/admin');
-//         } else if (storedData) {
-//             if (username === storedData.username && password === storedData.password) {
-//                 navigate('/user');
-//             }
-//             else{
-//                 alert('wrong input');
-//             }
-//         }
-//     }
-
-
-
-//     return (
-//         <div>
-//             <form onSubmit={submitHandler}>
-//                 <div className='loginform'>
-//             <h1>LOGIN FORM</h1><hr />
-//                 <label htmlFor="username">Username:</label>
-//                 <input type='text' name='username' value={username} onChange={changeHandler} placeholder='enter username' /><br />
-//                 <label htmlFor="password">Password:</label>
-//                 <input type='password' name='password' value={password} onChange={changeHandler} /><br />
-//                 <button className='btn btn-primary' type="submit">Login</button><br />
-//                 {/* <button className='btn btn-primary' onClick={registerHandler}>Register</button> */}
-//                 </div>
-//             </form>
-//         </div>
-//     )
-// }
-
-// export default Signin;
-
-
-
-
-
-import React, { useEffect } from 'react';
-import secondlogo from '../../Assets/Logo.png';
-import firstlogo from '../../Assets/Logo.png';
-import { Button } from 'react-bootstrap';
-
-function Signin() {  
-  useEffect(() => {
-     
-    document.body.style.overflow = 'hidden';
-    
-    return () => {
-      document.body.style.overflow = 'auto';
-    };
-  }, []);
-
+function Signin() {
   return (
-    <>
-      {/* First section */}
-      <section>
-        <div className='container-fluid'>
-          <div className='row'>
-            <div className='col-lg-6 col-md-6 login_first_section d-flex justify-content-center align-items-center my-5'>
-              <div>
-                <div style={{ textAlign: 'center' }}>
-                  <img src={firstlogo} alt={firstlogo} className='img-fluid d-md-none' />
-                </div>
-                <h1 className='welcome'>Welcome back</h1>
-                <h6 className='enteryourdetails'>Welcome back! Please enter your details.</h6>
-
+    <div className="container">
+      <div className="row justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+        <div className="col-md-8">
+          <Card className="w-100" style={{ borderRadius: '20px' }}>
+            <div className="row">
+              <div className="col-md-6 p-4">
+                <h4 className="text-center mb-2">Sign in</h4>
                 <form>
-                  <div>
-                    <label className='mb-2 mt-2 email'>Email</label>
-                    <br />
-                    <input type="email" placeholder='Enter Your email' className='emailinputfield mb-3' />
+                  <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                   </div>
-                  <div>
-                    <label className='mb-2 mt-2  password'>Password</label>
-                    <br />
-                    <input type="password" placeholder='Enter Your Password' className='emailinputfield mb-3' />
+                  <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" />
                   </div>
-
-                  <div className='my-3'>
-                    <div className=' d-flex  justify-content-between'>
-
-                      <div className='col-lg-7 d-flex'>
-                        <div>
-                          <input type="checkbox" />
-                          &nbsp; &nbsp;
-                        </div>
-                        <div>
-                          <p className='remember'>Remember for  password</p>
-                        </div>
-                        &nbsp; &nbsp;
-                      </div>
-                      <div className='col-lg-4'>
-                        <p className='remember remember1'>Forgot Password</p>
-                      </div>
-                    </div>
+                  <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
                   </div>
-
-                  <Button variant="success" size="lg" className="ps-5 pe-5 btn-lg w-100 mb-3">Sign in</Button>
-
-                  <Button variant="" size="lg" className="ps-5 pe-5 btn-lg w-100 signwithgoogle" style={{ border: '1px solid black' }}>
-                    Sign in with Google
-                  </Button>
-
-                  <p className='my-5'>Don't have an account? <span className='signup'>Sign up</span></p>
-
+                  <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
               </div>
+              <div className="col-md-6 green-background pt-5" style={{    textAlign: 'center' }}>
+                <h4 className="pt-5">Hello Friend!</h4>
+                <p>No results containing all your search terms were found. Your search - iueuehweufwe - did not match any documents.</p>
+              </div>
             </div>
-
-            <div className='col-lg-6 col-md-6 login_second_section d-none d-md-flex justify-content-center align-items-center'>
-              <img src={secondlogo} alt={secondlogo} className='img-fluid' />
-            </div>
-
-          </div>
+          </Card>
         </div>
-
-      </section>
-
-    </>
-  )
+      </div>
+    </div>
+  );
 }
 
 export default Signin;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // import React, { useState } from 'react';
-// // import { useNavigate } from 'react-router-dom';
-// // import './Sigin.css';
-
-// // const Signin = () => {
-// //     const navigate = useNavigate();
-// //     const [data, setData] = useState({
-// //         username: "",
-// //         password: "",
-// //     });
-
-// //     const { username, password } = data;
-
-// //     const changeHandler = (e) => {
-// //         setData({ ...data, [e.target.name]: e.target.value });
-// //     }
-
-// //     const Add = [
-// //         {
-// //             username: 'Admin',
-// //             password: 10101,
-// //         }
-// //     ];
-
-// //     const submitHandler = (event) => {
-// //         event.preventDefault();
-// //         const New = Add.find(user => user.username === username && user.password.toString() === password);
-// //         const storedData = JSON.parse(localStorage.getItem('userData'));
-
-// //         if (New) {
-// //             navigate('/User');
-// //         } else if (storedData) {
-// //             if (username === storedData.username && password === storedData.password) {
-// //                 navigate('/Client');
-// //             }
-// //             else{
-// //                 alert('wrong input');
-// //             }
-// //         }
-// //     }
-
-
-
-// //     return (
-// //         <div>
-// //             <div className="login">    
-// //                 <h4>Login</h4>
-// //                 <form onSubmit={submitHandler}>
-// //                 <div className="text_area">
-// //                     <label htmlFor="username">Username:</label>
-// //                     <input type='text' name='username' value={username} onChange={changeHandler} placeholder='enter username' /><br />
-// //                 </div>
-// //                 <div className="text_area">
-// //                     <label htmlFor="password">Password:</label>
-// //                     <input type='password' name='password' value={password} onChange={changeHandler} /><br />
-// //                     <input type="submit" value="LOGIN" className="btn" />
-// //                     {/* <button className='btn btn-primary' type="submit">Login</button><br /> */}
-// //                 </div>
-// //                 </form>
-// //             </div>
-           
-// //         </div>
-// //     )
-// // }
-
-// // export default Signin;
-
- 
